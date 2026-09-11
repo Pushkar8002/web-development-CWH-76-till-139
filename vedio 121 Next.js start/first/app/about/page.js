5a7170cab6a2f3172b4236b32b98513d1d5b45c7
Pushkar singh <pushkarsingh8002@gmail.com>
2026-09-11 13:46:55 +0530

    Add files via upload

---
+++ vedio 121 Next.js start/first/app/about/page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">About Us</h1>
+      <p>Welcome to our about page!</p> 
+      <ul>
+        <li>Full stack Solution</li>
+        <li>File based routing</li>
+        <li>additional features</li>
+        <li>SEO friendly</li>
+        <li>optimized Rendering</li>
+      </ul>
+    </div>
+  )
+}
+
+export default page
